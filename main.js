53fda5c8170599055a5796839f498f3a2531b912
Jeremia Lo <lojeremia@gmail.com>
2022-08-11 22:12:24 -0500

    opened door

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".home").hover(
+        function() {$(this).attr("src","images/house2.png");},
+        function() {$(this).attr("src","images/house.png");
+    });
+});
