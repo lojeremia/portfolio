cba174e9cab782ba1ef26e8c6b3f7d6205440474
Jeremia Lo <lojeremia@gmail.com>
2022-08-11 23:38:31 -0500

    cursor

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
     $(".home").hover(
-        function() {$(this).attr("src","images/house2.png");},
-        function() {$(this).attr("src","images/house.png");
+        function() {
+            $(this).attr("src","images/house2.png");
+        },
+        function() {
+            $(this).attr("src","images/house.png");
+    });
+
+    const cursor = document.querySelector(".cursor");
+    document.addEventListener("mousemove", function(e) {
+        cursor.style.left = e.pageX + "px";
+        cursor.style.top = e.pageY + "px";
     });
 });
+
