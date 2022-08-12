dba3e4159e74e0d18c154697faf58f8050a19b55
Jeremia Lo <lojeremia@gmail.com>
2022-08-12 13:53:27 -0500

    step inside

--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 $(document).ready(function(){
     $(".home").hover(
         function() {
-            $(this).attr("src","images/house2.png");
+            $(this).attr("src","images/index/house2.png");
         },
         function() {
-            $(this).attr("src","images/house.png");
-    });
-
-    const cursor = document.querySelector(".cursor");
-    document.addEventListener("mousemove", function(e) {
-        cursor.style.left = e.pageX + "px";
-        cursor.style.top = e.pageY + "px";
+            $(this).attr("src","images/index/house.png");
     });
 });
+
+$(document).mousemove(function(e) {
+
+    const cursor = $('.cursor');
+    const target = $(event.target);
+
+    cursor.css('left', e.clientX-10).css('top', e.clientY-10);
+    
+    const isLinkTag = target.is('a');
+    const isHovered = cursor.hasClass('cursorHover');
+    
+    if(isLinkTag && !isHovered) {
+    
+      cursor.addClass('cursorHover');
+  
+    } else if(!isLinkTag && isHovered) {
+    
+      cursor.removeClass('cursorHover');
+    
+    }
+    
+  });
+  
+  $(document).mouseleave(function(e) {
+  
+    const cursor = $('.cursor');
+    cursor.hide()
+  
+  });
+  
+  $(document).mouseenter(function(e) {
+  
+    const cursor = $('.cursor');
+    cursor.show()
+  
+  });
+
 
